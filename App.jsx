eba9e377d9557e91e4bfa486829809891580f8da
Herberth Andrade <herberth@x1tts.onmicrosoft.com>
2022-09-03 14:06:29 -0300

    initial commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Home from "./src/pages/Home";
+import { View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
